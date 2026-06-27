c5a31192a09f1dcc59875a9fe65bf7b1745e8561
minyinpop <minyinpop@gmail.com>
2026-06-27 23:21:25 +0800

    添加 v-for 生成按鈕

--- src/components/ModelSelector.vue
+++ src/components/ModelSelector.vue
@@ -1,18 +1,33 @@
 <script setup lang="ts">
-  import type {BotModel} from "@/types/BotModel.ts";
+  import type { ModelChangeButton } from "@/types/ModelChangeButton.ts";
+  import type { BotModel } from "@/types/BotModel.ts";
 
   const emit = defineEmits<{ change_model: [model: BotModel] }>()
 
   function on_click_change_model_button(model: BotModel) {
     emit("change_model", model)
   }
+
+  const models: ModelChangeButton[] = [
+    {
+      button_title: "切換至 OpenAI",
+      button_model: "openai"
+    },
+    {
+      button_title: "切換至 Ollama",
+      button_model: "ollama"
+    }
+  ]
 </script>
 
 <template>
   <div class="bot-model-change">
     <h2>更換模型</h2>
-    <button @click="on_click_change_model_button('openai')">切換至 OpenAI</button>
-    <button @click="on_click_change_model_button('ollama')">切換至 Ollama</button>
+    <button v-for="model in models"
+            v-bind:key="model.button_model"
+            v-on:click="on_click_change_model_button(model.button_model)">
+      {{ model.button_title }}
+    </button>
   </div>
 </template>
 
