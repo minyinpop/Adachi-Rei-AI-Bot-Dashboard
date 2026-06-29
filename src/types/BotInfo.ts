107568bfcd027bb47f9ef0d751260c25e2e7cac8
minyinpop <minyinpop@gmail.com>
2026-06-29 23:33:47 +0800

    換位置

--- src/types/BotInfo.ts
+++ src/types/BotInfo.ts
@@ -1,5 +1,5 @@
-import type { BotModel } from "@/types/BotModel.ts"
 import type { BotStatus } from "@/types/BotStatus.ts"
+import type { BotModel } from "@/types/BotModel.ts"
 
 export interface BotInfo {
   name: string
