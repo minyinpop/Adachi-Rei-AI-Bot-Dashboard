00b3da9ab2a1edcc5c564f7598b361f7edd9961d
minyinpop <minyinpop@gmail.com>
2026-06-29 20:48:52 +0800

    優化程式碼

--- src/composables/useBot/useBot.ts
+++ src/composables/useBot/useBot.ts
@@ -2,15 +2,22 @@ import { add_log } from "@/composables/useBot/log.ts"
 import type { BotModel } from "@/types/BotModel.ts"
 import type { BotInfo } from "@/types/BotInfo.ts"
 import { delay } from "@/utils/delay.ts"
-import { reactive } from "vue"
+import { reactive, watch } from "vue"
+
+const ai_bot = reactive<BotInfo>({
+  name: "足立レイ Discord Bot",
+  status: "closed",
+  model: "ollama"
+})
+
+watch(
+  () => ai_bot.status,
+  (new_status, old_status) => {
+    add_log(`${old_status} -> ${new_status}`)
+  }
+)
 
 export function useBot() {
-  const ai_bot = reactive<BotInfo>({
-    name: "足立レイ Discord Bot",
-    status: "closed",
-    model: "ollama"
-  })
-
   async function change_ai_model(model: BotModel)
   {
     if (model === ai_bot.model)
@@ -18,22 +25,16 @@ export function useBot() {
       return
     }
 
-    const is_openai = model === "openai"
-
-    add_log(`已關閉 ${model}`)
+    const is_openai: boolean = model === "openai"
 
     ai_bot.status = "closed"
     ai_bot.model = model
 
     await delay(500)
 
-    add_log(`載入 ${model} 中`)
-
     ai_bot.status = "loading"
 
     await delay(is_openai ? 1500 : 250)
-
-    add_log(`${model} 載入完成`)
 
     ai_bot.status = "ready"
   }
