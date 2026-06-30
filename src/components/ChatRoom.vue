90e76dc0b9710c3943d5626ec02f58315d8b2a1e
minyinpop <minyinpop@gmail.com>
2026-06-30 22:32:32 +0800

    搬移邏輯

--- src/components/ChatRoom.vue
+++ src/components/ChatRoom.vue
@@ -1,30 +1,20 @@
 <script setup lang="ts">
+  import { ask_bot } from "@/services/askBot.ts"
+  import type { Ref } from 'vue'
   import { ref } from "vue"
 
-  const message = ref<string>()
+  const message: Ref = ref<string>()
 
-  async function show_text() {
-    const response = await fetch("http://127.0.0.1:8000/chat", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        message: message.value
-      })
-    })
-
-    const data = await response.json()
-
-    message.value = data.message
+  async function on_click_ask_bot_button() {
+    message.value = await ask_bot(message.value)
   }
 </script>
 
 <template>
   <div class="chat-room">
     <input v-model="message">
-    <button v-on:click="show_text">
-      顯示文字
+    <button v-on:click="on_click_ask_bot_button">
+      發送文字
     </button>
   </div>
 </template>
